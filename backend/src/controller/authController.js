03da758cb981c6af8a545586f4e6292d8027c0a5
indranilmondal901 <indranilmondal901@gmail.com>
2023-04-09 12:32:12 +0530

    all working and tested except logout

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -1,22 +1,90 @@
-exports.registerController = async()=> {
+//collection
+const customer = require('../model/userSchema')
+//bcrypt
+const bcrypt = require('bcrypt');
+
+exports.registerController = async (req, res) => {
     try {
-        const {name,email,password} = req.body
-        //test it
+        const email = req.body.email;
+        const user = await customer.findOne({ email: email });
+        if (user) {
+            res.status(400).send({
+                message: "Email already exist"
+            })
+        } else {
+            const userData = new customer({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            })
+            //hashing before save
+            //save into db
+            console.log(userData);
+            await userData.save();
+            res.status(200).send({
+                status: "sucessfully registered",
+                data: userData
+            })
+        }
     } catch (error) {
-        
+        res.status(401).send({
+            message: error.message
+        })
     }
 }
-exports.loginController = async()=> {
+exports.loginController = async (req, res) => {
     try {
-        
-    } catch (error) {
-        
+        const enteredUsername = req.body.username;
+        const enteredPassword = req.body.password;
+
+        const loginUserData = await customer.findOne({ email: enteredUsername });
+
+        if (!loginUserData) {
+            res.status(500).send({
+                status: 500,
+                message: "User with the given email not found"
+            })
+            return;
+        }
+
+        const checkPassword = await bcrypt.compare(enteredPassword, loginUserData.password);
+        if (checkPassword) {
+            const token = await loginUserData.generateAuthToken(loginUserData);
+            console.log(` logintoken from rout.js==> ${token}`)
+            res.status(200).send({
+                status: "login successfully",
+                token: token,
+                user: loginUserData
+            })
+        } else {
+            res.status(401).send({
+                status: 401,
+                message: "Invalid credential"
+            })
+        }
+    } catch (err) {
+        res.status(500).send({
+            status: 500,
+            message: err.message
+        })
     }
 }
-exports.logoutController = async()=> {
+
+exports.logoutController = async (req, res) => {
     try {
-        
-    } catch (error) {
-        
+        console.log(`this user is logged out ==> ${req.user.name}`)
+        //logout from all devices
+        console.log(req.user.tokens)
+        req.user.tokens = [];
+        await req.user.save();
+        console.log(req.user.tokens)
+        res.status(200).send({
+            msg: "logout Sucessfully"
+        })
+    } catch (err) {
+        res.status(501).send({
+            msg: "err in logout",
+            err: err.message
+        })
     }
 }
--- backend/src/model/userSchema.js
+++ backend/src/model/userSchema.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
 
 //schema
 const userSchema = new mongoose.Schema({
@@ -23,6 +27,29 @@ const userSchema = new mongoose.Schema({
     }]
 })
 
+//hashing
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next()
+})
+
+//Token Generation
+userSchema.methods.generateAuthToken = async (loginUser) => {
+    console.log(loginUser);
+    try {
+        const token = jwt.sign({ _id: loginUser._id}, process.env.SECRET_KEY);
+        console.log(token);
+        loginUser.tokens = await loginUser.tokens.concat({ token: token })
+        await loginUser.save();
+        return token;
+    } catch (error) {
+        console.log("err in generating token==>" + error);
+        res.status(500).send(err);
+    }
+}
+
 //model
-const customer = new mongoose.model('customer',userSchema);
+const customer = mongoose.model('customer', userSchema);
 module.exports = customer;
